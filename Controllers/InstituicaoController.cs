3eea1e1965e03eb152cf364240ac2f0db8449e20
luisahug <0356177@feevale.br>
2024-09-06 15:09:50 -0300

    add

--- Controllers/InstituicaoController.cs
+++ Controllers/InstituicaoController.cs
@@ -28,7 +28,7 @@ namespace Capitulo01.Controllers
                 return NotFound();
             }
 
-            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(m => m.InstituicaoID == id);
+            var instituicao = await _context.Instituicoes.Include(d => d.Departamentos).SingleOrDefaultAsync(m => m.InstituicaoID == id);
             if (instituicao == null)
             {
                 return NotFound();
