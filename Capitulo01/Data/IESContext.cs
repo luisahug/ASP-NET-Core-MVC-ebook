a28343274b35fbecafebc9b23fdccd232e218241
luisahug <0356177@feevale.br>
2024-09-09 11:18:08 -0300

    ajuste erro que nao permitia ver detalhes instituicoes, ainda com erro em create e edit

---
+++ Capitulo01/Data/IESContext.cs
@@ -0,0 +1,24 @@
+﻿using Capitulo01.Models;
+using Microsoft.EntityFrameworkCore;
+using Modelo.Cadastros;
+
+namespace Capitulo01.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+            { 
+            }
+        }
+    }
+}
---
+++ Capitulo01/Data/IESDbInitializer.cs
@@ -0,0 +1,70 @@
+﻿using Capitulo01.Models;
+using System.Linq;
+using Modelo.Cadastros;
+
+namespace Capitulo01.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+			context.Database.EnsureDeleted();
+			context.Database.EnsureCreated();
+
+            if (context.Departamentos.Any())
+            {
+                return;
+            }
+
+            var instituicoes = new Instituicao[]
+            {
+				new Instituicao()
+				{
+					Nome = "UFRGS",
+					Endereco = "Porto Alegre"
+				},
+				new Instituicao()
+				{
+					Nome = "Feevale",
+					Endereco = "Novo Hamburgo"
+				},
+				new Instituicao()
+				{
+					Nome = "Unisinos",
+					Endereco = "São Leopoldo"
+				},
+				new Instituicao()
+				{
+					Nome = "Faccat",
+					Endereco = "Taquara"
+				},
+				new Instituicao()
+				{
+					Nome = "PUCRS",
+					Endereco = "Porto Alegre"
+				}
+			};
+
+			foreach (Instituicao i in instituicoes)
+			{
+				context.Instituicoes.Add(i);
+			}
+
+
+			context.SaveChanges(); //primeiro salvar os novos dados de instituicao antes de relacioná-los com departamentos
+
+			var departamentos = new Departamento[]
+			{
+				new Departamento {Nome="Ciência da Computação", InstituicaoID = instituicoes[0].InstituicaoID},
+				new Departamento {Nome="Engenharia de Software", InstituicaoID = instituicoes[1].InstituicaoID}
+			};
+
+			foreach (Departamento d in departamentos)
+			{
+				context.Departamentos.Add(d);
+			}
+
+			context.SaveChanges();
+		}
+    }
+}
