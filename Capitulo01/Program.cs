f75517b7d946a65e84f122be2eb4bbd052ac3e38
luisahug <0356177@feevale.br>
2024-09-12 15:54:54 -0300

    add rota default home

--- Capitulo01/Program.cs
+++ Capitulo01/Program.cs
@@ -27,6 +27,11 @@ app.MapControllerRoute(
     name: "areaRoute",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 /*using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
